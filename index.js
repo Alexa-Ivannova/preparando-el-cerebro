930502469efc701dd0881d4ec9f08c323c3651b8
Alexa Bautista <alexa.bautista@genstudents.org>
2024-08-23 08:11:18 -0500

    ejercicio nuevo arreglo

--- index.js
+++ index.js
@@ -51,3 +51,19 @@ sumatoria(numberSum);
 // console.log(sumatoria(numberSum));
 
 
+// NUEVO ARREGLO
+// Deberás crear una función llamada nuevoArreglo que reciba un número como parámetro y que devuelva un nuevo arreglo con tantos elementos como el número que le hayas pasado. Ejemplo:
+// nuevoArreglo(5) debe retornar [1,2,3,4,5]
+// nuevoArreglo(10) debe retornar [1,2,3,4,5,6,7,8,9,10]
+
+const numArray = 5;
+
+const nuevoArreglo = (numberArray) => {
+  const array = [];
+  for (let i = 1; i <= numberArray; i++) {
+    array.push(i)
+  }
+  return array
+};
+nuevoArreglo(numArray);
+console.log(nuevoArreglo(numArray));
